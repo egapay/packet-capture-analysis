ddda094719ccbc711a9d3d44d61d7e2a2e3e233a
dinga68 <81820605+dinga68@users.noreply.github.com>
2021-11-18 15:02:48 -0500

    Update packet_parser.py

--- packet_parser.py
+++ packet_parser.py
@@ -1,4 +1,36 @@
-def parse() :
+def parse(afile, data):
 	print('called parse function in packet_parser.py')
+	infile = open(afile, 'r')
 
+	line = infile.readline().strip()
+	x = 0
+	while line:
+		test = ' '.join(line.split())		
+		data.append(test.split(' '))	
+		line = infile.readline().strip()
+		
+	infile.close()
 	
+	
+	
+
+node1 = []
+node2 = []
+node3 = []
+node4 = []
+
+for i in range(0, 3):
+	if(i == 0):
+		#afile = 'Node{}_filtered.txt'.format(i)
+		afile = 'test.txt'
+		parse(afile, node1)
+	#if(i == 1):
+		#afile = 'Node{}_filtered.txt'.format(i)
+		#parse(afile, node2)
+	#if(i == 2):
+		#afile = 'Node{}_filtered.txt'.format(i)
+		#parse(afile, node3)
+	#if(i == 3):
+		#afile = 'Node{}_filtered.txt'.format(i)
+		#parse(afile, node4)
+print(node1[0][6])
