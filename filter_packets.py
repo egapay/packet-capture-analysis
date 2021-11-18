305b3a6704d95453f5f4c7ef1659e57ae842a213
unknown <etg5588@rit.edu>
2021-11-18 15:39:23 -0500

    updated filter_packets.py, code able to filter ICMP packets from Node(1-4).txt and outputs Node(1-4)_filtered.txt

--- filter_packets.py
+++ filter_packets.py
@@ -1,22 +1,33 @@
-def filter(input_file):
-	file1 = open(input_file, 'r')
-	file_lines = file1.readlines()
-	print(file_lines)
+def filter(file):
+	#open file and read number of lines
+	count_file = open(file, 'r')
+	file_lines = count_file.readlines()
 	num_lines = len(file_lines)
-	print(num_lines)
-	line = file1.readline()
-	i = 0
-	while i < num_lines:
-		word = "ICMP"
-		line = line.strip()
-		line = file1.readline().strip()
-		if i < 20:
-			print(line)
-		i = i + 1
-		if word in line:
-			print(line)
+	count_file.close()
+
+	#open file to read lines into output file
+	input_file = open(file, 'r')
+	#changing output file name based off original passed through file
+	output_file_name = 'Node' + file[4:5] + '_filtered.txt'
+	output_file = open(output_file_name, 'w')
+	count = 0
+
+	#read in file depending on amount of lines from file
+	line = input_file.readline()
+	while count < num_lines:
+		#checks for keyword
+		check = "ICMP"
+		line.strip()
+		if check in line:
+			# if keyword exists, write out line to file
+			output_file.write(line)
+		line = input_file.readline()
+		count += 1
 	
 
-filter("Node1.txt");
+filter("Node1.txt")
+filter("Node2.txt")
+filter("Node3.txt")
+filter("Node4.txt")
 
 
