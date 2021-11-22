e1517a72fa7a2d82a601b1bed84a3ae7818f938e
dinga68 <81820605+dinga68@users.noreply.github.com>
2021-11-22 16:28:37 -0500

    Update packet_parser.py

--- packet_parser.py
+++ packet_parser.py
@@ -19,18 +19,17 @@ node2 = []
 node3 = []
 node4 = []
 
-for i in range(0, 3):
-	if(i == 0):
-		#afile = 'Node{}_filtered.txt'.format(i)
-		afile = 'test.txt'
+for i in range(1, 5):
+	if(i == 1):
+		afile = 'Node{}_filtered.txt'.format(i)
 		parse(afile, node1)
-	#if(i == 1):
-		#afile = 'Node{}_filtered.txt'.format(i)
-		#parse(afile, node2)
-	#if(i == 2):
-		#afile = 'Node{}_filtered.txt'.format(i)
-		#parse(afile, node3)
-	#if(i == 3):
-		#afile = 'Node{}_filtered.txt'.format(i)
-		#parse(afile, node4)
-print(node1[0][6])
+	if(i == 2):
+		afile = 'Node{}_filtered.txt'.format(i)
+		parse(afile, node2)
+	if(i == 3):
+		afile = 'Node{}_filtered.txt'.format(i)
+		parse(afile, node3)
+	if(i == 4):
+		afile = 'Node{}_filtered.txt'.format(i)
+		parse(afile, node4)
+print(node4[0][0])
