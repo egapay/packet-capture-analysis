5543779948b81d3bdab551fd70e921b1191914f6
unknown <etg5588@rit.edu>
2021-11-30 14:15:31 -0500

    changed filter_packets.py to only have method which is run in packet_analyzer.py

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -2,6 +2,10 @@ from filter_packets import *
 from packet_parser import *
 from compute_metrics import *
 
-filter()
+filter("Node1.txt")
+filter("Node2.txt")
+filter("Node3.txt")
+filter("Node4.txt")
+
 parse()
 compute()
