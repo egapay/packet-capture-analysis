2abae43688dd6731ce2def518909027931a97450
Student <student@localhost.localdomain>
2021-11-30 14:56:17 -0500

    removed what was outside of the function

--- packet_parser.py
+++ packet_parser.py
@@ -14,23 +14,6 @@ def parse(afile, data):
 	
 	
 
-node1 = []
-node2 = []
-node3 = []
-node4 = []
 
-for i in range(1, 5):
-	if(i == 1):
-		afile = 'Node{}_filtered.txt'.format(i)
-		parse(afile, node1)
-	if(i == 2):
-		afile = 'Node{}_filtered.txt'.format(i)
-		parse(afile, node2)
-	if(i == 3):
-		afile = 'Node{}_filtered.txt'.format(i)
-		parse(afile, node3)
-	if(i == 4):
-		afile = 'Node{}_filtered.txt'.format(i)
-		parse(afile, node4)
 
 
