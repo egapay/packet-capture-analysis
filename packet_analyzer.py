6066d60a6a7b62eba67b582de9c41e2287ecadb7
Student <student@localhost.localdomain>
2021-11-30 14:57:58 -0500

    adding to the parse function

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -3,5 +3,24 @@ from packet_parser import *
 from compute_metrics import *
 
 filter()
-parse()
+parse(
+node1 = []
+node2 = []
+node3 = []
+node4 = []
+
+for i in range(1, 5):
+	if(i == 1):
+		afile = 'Node{}_filtered.txt'.format(i)
+		parse(afile, node1)
+	if(i == 2):
+		afile = 'Node{}_filtered.txt'.format(i)
+		parse(afile, node2)
+	if(i == 3):
+		afile = 'Node{}_filtered.txt'.format(i)
+		parse(afile, node3)
+	if(i == 4):
+		afile = 'Node{}_filtered.txt'.format(i)
+		parse(afile, node4)
+)
 compute()
