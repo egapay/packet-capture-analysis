9455428fc1405af7a88774583fdf9024a7d8e046
unknown <etg5588@rit.edu>
2021-11-30 14:19:57 -0500

    reverted last commit

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -2,10 +2,6 @@ from filter_packets import *
 from packet_parser import *
 from compute_metrics import *
 
-filter("Node1.txt")
-filter("Node2.txt")
-filter("Node3.txt")
-filter("Node4.txt")
-
+filter()
 parse()
 compute()
