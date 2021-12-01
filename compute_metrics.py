ab141d2dce8ea4d908a48a7313d1311e6d7f5df5
Cannonplus2 <82120924+Cannonplus2@users.noreply.github.com>
2021-12-01 11:02:25 -0500

    Add files via upload

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,60 +1,68 @@
 def compute(List, node) :
   #Metrics To Calculate
   i = 0
+  String =  ' '
   SentReq = 0
   RecReq = 0
   SentRep = 0
   RecRep = 0
+  TotalReqSent = 0
+  TotalReqRec = 0
+  DataReqSent = 0
+  DataReqRec = 0
   source = 0
+ 
   #Set Which Source IP to use based on what Node we getting info from
   if node == 1:
-    source = "192.168.100.1"
+     source = "192.168.100.1"
   if node == 2:
-    source = "192.168.100.2"
+     source = "192.168.100.2"
   if node == 3:
-    source = "192.168.200.1"
+     source = "192.168.200.1"
   if node == 4:
-    source = "192.168.200.2"
-  reply = "Echo (ping) reply    id=0x0001"
-  request = "Echo (ping) request id=0x0001" 
+     source = "192.168.200.2"
+  reply = "reply"
+  request = "request" 
   #For Loop To Iterate Through Info List
   for x in List:
-    #print(L[i][6] == reply)
-    print(List[i][2] == source)
     #Calculate The Amount of Requests Sent
-    if List[i][2] == source and List[i][6] == request:
+    if List[i][2] == source and List[i][8] == request:
      SentReq = SentReq + 1 
-     print( "Amount of Requests Sent = " + str(SentReq))
-    #Calculate The Amount of Requests Recieved
-    if List[i][3] == source and List[i][6] == request:
-     RecReq = RecReq + 1 
-     print("Amount of Requests Recieved = " + str(RecReq))
-    #Calculate The Amount of Replies sent
-    if List[i][2] == source and List[i][6] == reply:
-      SentRep = SentRep + 1 
-      print("Amount of Replies Sent = " + str(SentRep))
-    #Calculate The Amount of Replies Recieved
-    if List[i][3] == source and List[i][6] == reply:
-     RecRep = RecRep + 1 
-     print("Amount of Replies Recieved = " + str(RecRep))
-
-    print(List[i][2])
-    i= i + 1	
+    #Total Echo Requests Bytes Sent
+     TotalReqSent =  TotalReqSent + int(List[i][5]) 
+     DataReqSent =  DataReqSent + (int(List[i][5])-32)
   
-  print(SentReq)
-  print(RecReq)
-  print(SentRep)
-  print(RecRep)
+    #Calculate The Amount of Requests Recieved
+    if List[i][3] == source and List[i][8] == request:
+     TotalReqRec =  TotalReqRec + int(List[i][5]) 
+     DataReqRec =  DataReqRec + (int(List[i][5])-32)
 
-  #print(str((float(List[0][5]) - 28)/(float(List[0][1]))))
-  #Echo Request Goodput & average reply delay
-  value = 0
-  while value < len(List):
-    if(List[value][8] == "request"): #checks to make sure that it is a request packet
-      goodput = format(((float(List[value][5]) - 28)/1000)/(float(List[value][1])), ".5f") #sets the output variable to the length of the packet - 28 and divdes it by 1000 (kB/sec then divides it by the time
-      if(List[value + 1][8] == "reply"): #this makes sure that the next packet is reply packet
-          delay = (float(List[value][1]) - float(List[value + 1][1]) * 1000000) # takes the value of the request packet and subtracts it from the reply time then multiply by 1000000(microseconds)
-          #print(str(delay))
-      print(goodput)
+     RecReq = RecReq + 1 
+     
+    #Calculate The Amount of Replies sent
+    if List[i][2] == source and List[i][8] == reply:
+      SentRep = SentRep + 1 
+      
+    #Calculate The Amount of Replies Recieved
+    if List[i][3] == source and List[i][8] == reply:
+     RecRep = RecRep + 1 
+    
 
-    value += 1
+    
+    i= i + 1
+
+  print( "Amount of Requests Sent = " + str(SentReq))	
+  print("Amount of Requests Recieved = " + str(RecReq))
+  print("Amount of Replies Sent = " + str(SentRep))
+  print("Amount of Replies Recieved = " + str(RecRep))
+
+  print(TotalReqSent)
+  print(TotalReqRec)
+  print(DataReqSent)
+  print(DataReqRec)
+
+
+ 
+L = [["1","0.000000","192.168.200.1","192.168.100.1", "ICMP", "74","Echo (ping) request id=0x0001","seq=14/3584", "ttl=128 (reply in 2)"], 
+["2", "0.003678" , "192.168.100.1", "192.168.200.1", "ICMP", "74" , "Echo (ping) reply    id=0x0001", "seq=14/3584", "ttl=126 (request in 1)"]]
+
