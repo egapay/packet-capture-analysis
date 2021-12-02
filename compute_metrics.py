947416cd621e7634521b22c28367f7cd6a2408d1
Ian Dinga <iandinga@Ians-MacBook.local>
2021-12-02 14:26:51 -0500

    Timebased metrics 3 and 4

--- compute_metrics.py
+++ compute_metrics.py
@@ -83,12 +83,19 @@ def compute(List, node) :
    print("RTT = " + str(rtt))
   #print(str((float(List[0][5]) - 28)/(float(List[0][1]))))
   #Echo Request Goodput & average reply delay
+   averagereply = 0
    value = 0
+   counter=0
+   goodput = 0
    while(value < len(List)):
-      if(List[value][8] == "request"): #checks to make sure that it is a request packet
-         goodput = format(((float(List[value][5]) - 28)/1000)/(float(List[value][1])), ".5f") #sets the output variable to the length of the packet - 28 and divdes it by 1000 (kB/sec then divides it by the time
+      if(List[value][8] == "request"): #checks to make sure that it is a request packet(float(List[value][1]))
+         goodput += (float(List[value][5]) - 28)/1000
          if(List[value + 1][8] == "reply"): #this makes sure that the next packet is reply packet
-            delay = (float(List[value][1]) - float(List[value + 1][1]) * 1000000) # takes the value of the request packet and subtracts it from the reply time then multiply by 1000000(microseconds)
-            print(str(delay))
-         print(goodput)
+            delay = format((float(List[value+1][1]) - float(List[value][1])) * 1000000, ".2f") # takes the value of the request packet and subtracts it from the reply time then multiply by 1000000(microseconds)
+            averagereply += float(delay)
+            counter += 1
+         #print(goodput)
       value += 1
+   goodput = goodput/1000
+   print(goodput/TimeNano)
+   print(format(averagereply/counter, ".3f"))
